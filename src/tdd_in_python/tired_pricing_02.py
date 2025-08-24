a773c30aea224088a4f3f0087d7be9cfeb108c43
sebasbit <sebastian.torres0426@gmail.com>
2025-08-24 17:32:10 -0500

    refactor(blue): simplify chained comparison

--- src/tdd_in_python/tired_pricing_02.py
+++ src/tdd_in_python/tired_pricing_02.py
@@ -1,9 +1,9 @@
 def calculate_tired_pricing(quantity: int) -> int:
-    if quantity >= 3 and quantity <= 10:
+    if 3 <= quantity <= 10:
         return quantity * 239
-    elif quantity >= 11 and quantity <= 25:
+    elif 11 <= quantity <= 25:
         return quantity * 219
-    elif quantity >= 26 and quantity <= 50:
+    elif 26 <= quantity <= 50:
         return quantity * 199
     elif quantity > 50:
         return quantity * 149
