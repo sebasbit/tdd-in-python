3298c5be4c33326fe660a303fb2109eb70d6bb8f
sebasbit <sebastian.torres0426@gmail.com>
2025-08-24 14:00:58 -0500

    refactor(blue): parametrize tests

--- tests/test_product_01.py
+++ tests/test_product_01.py
@@ -1,6 +1,11 @@
+import pytest
+
 from tdd_in_python import Product
 
 
-def test_product_price():
-    product = Product(10_000)
-    assert product.total() == 11_900
+@pytest.mark.parametrize(
+    "price,total", [(10_000, 11_900), (2_000, 2_380), (1_423_500, 1_693_965)]
+)
+def test_product_price(price, total):
+    product = Product(price)
+    assert product.total() == total
