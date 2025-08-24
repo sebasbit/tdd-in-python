7a93efa0b38b6a3b4eb4d302dd33daf7edb1f785
sebasbit <sebastian.torres0426@gmail.com>
2025-08-24 14:14:07 -0500

    refactor(blue): parametrize with a price of 0

--- tests/test_product_01.py
+++ tests/test_product_01.py
@@ -5,7 +5,7 @@ from tdd_in_python import Product
 
 
 @pytest.mark.parametrize(
-    "price,total", [(10_000, 11_900), (2_000, 2_380), (1_423_500, 1_693_965)]
+    "price,total", [(0, 0), (10_000, 11_900), (1_423_500, 1_693_965)]
 )
 def test_product_price(price, total):
     product = Product(price)
