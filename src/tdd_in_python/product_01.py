f4db9314c677af130ef0e0824fe8216152fac40f
sebasbit <sebastian.torres0426@gmail.com>
2025-08-24 16:32:29 -0500

    refactor(blue): add typing

--- src/tdd_in_python/product_01.py
+++ src/tdd_in_python/product_01.py
@@ -1,15 +1,15 @@
 class Product:
     VAT = 0.19
 
-    def __init__(self, price):
+    def __init__(self, price: "ProductPrice") -> None:
         self.price = price
 
-    def total(self):
+    def total(self) -> float:
         return self.price.value + (self.price.value * self.VAT)
 
 
 class ProductPrice:
-    def __init__(self, value):
+    def __init__(self, value: float) -> None:
         self.value = value
         if self.value < 0:
             raise InvalidPriceError()
