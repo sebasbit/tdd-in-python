4594e39c7dad3bf3938a83ef4a943333020037e6
sebasbit <sebastian.torres0426@gmail.com>
2025-08-24 13:38:06 -0500

    refactor(blue): extract constant

--- src/tdd_in_python/product_01.py
+++ src/tdd_in_python/product_01.py
@@ -1,6 +1,8 @@
 class Product:
+    VAT = 0.19
+
     def __init__(self, price):
         self.price = price
 
     def total(self):
-        return self.price + (self.price * 0.19)
+        return self.price + (self.price * self.VAT)
