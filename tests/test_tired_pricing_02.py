e207503b9805390e15b1f039160cfd2bac2baad0
sebasbit <sebastian.torres0426@gmail.com>
2025-08-24 17:13:43 -0500

    refactor(blue): parametrize tests

--- tests/test_tired_pricing_02.py
+++ tests/test_tired_pricing_02.py
@@ -3,15 +3,13 @@ Check out Codely's repository for a full explanation:
     https://github.com/CodelyTV/refactoring-code_smells-design_patterns/tree/de397ea9e331215aa8e5c9ea4e5ff16270a9ba0f/exercises/tiered_pricing
 """
 
+import pytest
+
 from tdd_in_python import calculate_tired_pricing
 
 
-def test_calculate_tired_pricing_for_range_1_to_2():
-    assert calculate_tired_pricing(1) == 299
-    assert calculate_tired_pricing(2) == 598
-
-
-def test_calculate_tired_pricing_for_range_3_to_10():
-    assert calculate_tired_pricing(3) == 717
-    assert calculate_tired_pricing(5) == 1195
-    assert calculate_tired_pricing(10) == 2390
+@pytest.mark.parametrize(
+    "quantity,tired_price", [(1, 299), (2, 598), (3, 717), (4, 956), (5, 1195)]
+)
+def test_calculate_tired_pricing(quantity, tired_price):
+    assert calculate_tired_pricing(quantity) == tired_price
