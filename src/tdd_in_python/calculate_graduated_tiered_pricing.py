e23e3ed0cda00f6c74ca5a2b0d7759e6e637377b
sebasbit <sebastian.torres0426@gmail.com>
2025-08-25 11:45:23 -0500

    refactor(blue): extract variables for tier 2

--- src/tdd_in_python/calculate_graduated_tiered_pricing.py
+++ src/tdd_in_python/calculate_graduated_tiered_pricing.py
@@ -2,14 +2,21 @@ def calculate_graduated_tiered_pricing(number):
     tier_1_price = 299
     tier_1_total = 2 * tier_1_price
 
+    tier_2_price = 239
+    tier_2_total = 8 * tier_2_price
+
     if number >= 51:
         return (
-            tier_1_total + (8 * 239) + (15 * 219) + (25 * 199) + ((number - 50) * 149)
+            tier_1_total
+            + tier_2_total
+            + (15 * 219)
+            + (25 * 199)
+            + ((number - 50) * 149)
         )
     if number >= 26:
-        return tier_1_total + (8 * 239) + (15 * 219) + ((number - 25) * 199)
+        return tier_1_total + tier_2_total + (15 * 219) + ((number - 25) * 199)
     if number >= 11:
-        return tier_1_total + (8 * 239) + ((number - 10) * 219)
+        return tier_1_total + tier_2_total + ((number - 10) * 219)
     if number >= 3:
-        return tier_1_total + ((number - 2) * 239)
+        return tier_1_total + ((number - 2) * tier_2_price)
     return number * tier_1_price
