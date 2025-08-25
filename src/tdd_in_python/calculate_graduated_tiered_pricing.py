a53d421d04ddf26141e41e72fa95d1868cdaaa5f
sebasbit <sebastian.torres0426@gmail.com>
2025-08-25 12:51:51 -0500

    refactor(blue): typing

--- src/tdd_in_python/calculate_graduated_tiered_pricing.py
+++ src/tdd_in_python/calculate_graduated_tiered_pricing.py
@@ -1,28 +1,28 @@
 class Tier:
-    def __init__(self, min_subs, max_subs, price):
+    def __init__(self, min_subs: int, max_subs: int, price: int) -> None:
         self.min_subs = min_subs
         self.max_subs = max_subs
         self.price = price
 
-    def apply_for(self, n_subs):
+    def apply_for(self, n_subs: int) -> bool:
         return self.min_subs <= n_subs
 
-    def cost_for(self, n_subs):
+    def cost_for(self, n_subs: int) -> int:
         if n_subs < self.min_subs:
             return 0
         if n_subs > self.max_subs:
             return self.price * self.total_subs_in_tier()
         return self.price * self.subs_in_tier(n_subs)
 
-    def subs_in_tier(self, n_subs):
+    def subs_in_tier(self, n_subs: int) -> int:
         return n_subs - self.min_subs + 1
 
-    def total_subs_in_tier(self):
+    def total_subs_in_tier(self) -> int:
         total_subs_in_tier = self.max_subs - self.min_subs + 1
         return total_subs_in_tier
 
 
-def calculate_graduated_tiered_pricing(subscriptions):
+def calculate_graduated_tiered_pricing(subscriptions: int) -> int:
     tiers = (
         Tier(1, 2, 299),
         Tier(3, 10, 239),
