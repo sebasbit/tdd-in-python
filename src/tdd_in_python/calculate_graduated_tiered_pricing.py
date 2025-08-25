fdca805f3d09e413fb4d7eb6e429eb5475d35a45
sebasbit <sebastian.torres0426@gmail.com>
2025-08-25 11:34:14 -0500

    feat(green): calculate subs price for tier 5

--- src/tdd_in_python/calculate_graduated_tiered_pricing.py
+++ src/tdd_in_python/calculate_graduated_tiered_pricing.py
@@ -1,4 +1,6 @@
 def calculate_graduated_tiered_pricing(number):
+    if number >= 51:
+        return (2 * 299) + (8 * 239) + (15 * 219) + (25 * 199) + ((number - 50) * 149)
     if number >= 26:
         return (2 * 299) + (8 * 239) + (15 * 219) + ((number - 25) * 199)
     if number >= 11:
