abed69df265f88506f6b76be874842ca6719a29a
sebasbit <sebastian.torres0426@gmail.com>
2025-08-25 11:59:45 -0500

    refactor(blue): introduce class Tier

--- src/tdd_in_python/calculate_graduated_tiered_pricing.py
+++ src/tdd_in_python/calculate_graduated_tiered_pricing.py
@@ -1,6 +1,14 @@
+class Tier:
+    def __init__(self, max_subs, price):
+        self.max_subs = max_subs
+        self.price = price
+
+    def total_cost(self):
+        return self.max_subs * self.price
+
+
 def calculate_graduated_tiered_pricing(number):
-    tier_1_price = 299
-    tier_1_total = 2 * tier_1_price
+    tier_1 = Tier(2, 299)
 
     tier_2_price = 239
     tier_2_total = 8 * tier_2_price
@@ -15,7 +23,7 @@ def calculate_graduated_tiered_pricing(number):
 
     if number >= 51:
         return (
-            tier_1_total
+            tier_1.total_cost()
             + tier_2_total
             + tier_3_total
             + tier_4_total
@@ -23,10 +31,13 @@ def calculate_graduated_tiered_pricing(number):
         )
     if number >= 26:
         return (
-            tier_1_total + tier_2_total + tier_3_total + ((number - 25) * tier_4_price)
+            tier_1.total_cost()
+            + tier_2_total
+            + tier_3_total
+            + ((number - 25) * tier_4_price)
         )
     if number >= 11:
-        return tier_1_total + tier_2_total + ((number - 10) * tier_3_price)
+        return tier_1.total_cost() + tier_2_total + ((number - 10) * tier_3_price)
     if number >= 3:
-        return tier_1_total + ((number - 2) * tier_2_price)
-    return number * tier_1_price
+        return tier_1.total_cost() + ((number - 2) * tier_2_price)
+    return number * tier_1.price
