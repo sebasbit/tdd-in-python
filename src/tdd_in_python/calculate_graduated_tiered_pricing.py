ace194eff0df9e7e86c7c4f1f02dba97e9516fe5
sebasbit <sebastian.torres0426@gmail.com>
2025-08-25 12:04:42 -0500

    refactor(blue): replace vars with class for tier 5

--- src/tdd_in_python/calculate_graduated_tiered_pricing.py
+++ src/tdd_in_python/calculate_graduated_tiered_pricing.py
@@ -12,8 +12,7 @@ def calculate_graduated_tiered_pricing(number):
     tier_2 = Tier(8, 239)
     tier_3 = Tier(15, 219)
     tier_4 = Tier(25, 199)
-
-    tier_5_price = 149
+    tier_5 = Tier(50, 149)
 
     if number >= 51:
         return (
@@ -21,7 +20,7 @@ def calculate_graduated_tiered_pricing(number):
             + tier_2.total_cost()
             + tier_3.total_cost()
             + tier_4.total_cost()
-            + ((number - 50) * tier_5_price)
+            + ((number - 50) * tier_5.price)
         )
     if number >= 26:
         return (
