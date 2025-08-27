f003eadee0944ed2f1f25378e894bdd6adf2247f
sebasbit <sebastian.torres0426@gmail.com>
2025-08-27 10:19:13 -0500

    test(red): [unit] validate VideoCreator removes final dots

--- tests/outside_in_04/test_video_creator.py
+++ tests/outside_in_04/test_video_creator.py
@@ -6,3 +6,10 @@ def test_video_creator_removes_trailing_and_leading_spaces():
     video_creator = VideoCreator()
     video = video_creator.execute(title)
     assert video.title == "title with spaces"
+
+
+def test_video_creator_removes_final_dots():
+    title = "title with a final dot."
+    video_creator = VideoCreator()
+    video = video_creator.execute(title)
+    assert video.title == "title with a final dot"
